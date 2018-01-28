149c7b606ee6d328765ed7235a80446019989ba3
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-27 22:21:04 -0200

    Renders a view using the Pug template engine

--- program.js
+++ program.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.static(process.argv[3]));
+app.use(express.static(process.argv[3] || path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "pug");
+
+app.get("/home", (req, res) => {
+  res.render("index", { date: new Date().toDateString() });
+});
 
 app.listen(process.argv[2]);
