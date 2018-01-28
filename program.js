73f7c574cd5c44d7c142c5da444ee2b225739c64
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-27 22:32:55 -0200

    Serves and processes a form

--- program.js
+++ program.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyparser = require("body-parser");
 
 app.use(express.static(process.argv[3] || path.join(__dirname, "public")));
+app.use(bodyparser.urlencoded());
 
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "pug");
 
 app.get("/home", (req, res) => {
   res.render("index", { date: new Date().toDateString() });
+});
+
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+
+app.post("/form", (req, res) => {
+  res.end(
+    req.body.str
+      .split("")
+      .reverse()
+      .join("")
+  );
 });
 
 app.listen(process.argv[2]);
