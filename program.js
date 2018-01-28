0557a83d7d547c84c415a55ea584dc84c3cf1625
Fábio Nascimento <fbn4sc@gmail.com>
2018-01-28 00:00:39 -0200

    Processes a url parameter

--- program.js
+++ program.js
@@ -35,4 +35,15 @@ app.post("/form", (req, res) => {
   );
 });
 
+app.put("/message/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.end(
+    require("crypto")
+      .createHash("sha1")
+      .update(new Date().toDateString() + id)
+      .digest("hex")
+  );
+});
+
 app.listen(process.argv[2]);
